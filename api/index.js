7d55defb44f0ff05b456e37c751e151d655cef16
tphilippini <t.philippini@hotmail.fr>
2023-03-14 20:50:38 +0100

    :sparkles: feat(api): adds express jwt

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 "use strict";
 
-// import authErrorMidd from "./_src/middlewares/authError.js";
+import authErrorMidd from "./_src/middlewares/authError.js";
 import authRouter from "./_src/auth/auth.route.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import corsMidd from "./_src/middlewares/cors.js";
 import dotenv from "dotenv";
 import express from "express";
+import expressJwt from "express-jwt";
 import helmet from "helmet";
 import matchRouter from "./_src/matches/match.route.js";
 import mongoose from "mongoose";
@@ -50,17 +51,19 @@ app.use(passport.initialize());
 //   }).unless({
 //     path: [
 //       {
-//         url: `${process.env.API_VERSION}/users`,
-//         methods: ['OPTIONS', 'POST'],
+//         url: `api/users`,
+//         methods: ["OPTIONS", "POST"],
 //       },
-//       `${process.env.API_VERSION}/auth`,
-//       `${process.env.API_VERSION}/auth/token`,
-//       // `${process.env.API_VERSION}/auth/google/token`,
-//       // `${process.env.API_VERSION}/auth/facebook/token`,
-//       `${process.env.API_VERSION}/auth/forgot`,
-//       `${process.env.API_VERSION}/auth/reset`,
-//       `${process.env.API_VERSION}/auth/validate`,
-//       // `${process.env.API_VERSION}/users/test`,
+//       `api`,
+//       `api/auth`,
+//       `api/auth/token`,
+//       // `api/auth/google/token`,
+//       // `api/auth/facebook/token`,
+//       `api/auth/forgot`,
+//       `api/auth/reset`,
+//       `api/auth/confirm`,
+//       `api/auth/validate`,
+//       // `api/users/test`,
 //     ],
 //   })
 // );
